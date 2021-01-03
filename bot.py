f636308e32bdc2a9f0fe8e3d2e2abcf7e83fb13b
Heinsteinberg <heinsteinberg@korea.ac.kr>
2021-01-03 14:42:29 +0900

    2021.01.03. 14:42

--- bot.py
+++ bot.py
@@ -10,8 +10,7 @@ recommendedLst = []
 @client.event
 async def on_ready():
     print('Initiating \'%s\'' % client.user.name)
-    # await client.change_presence(status=discord.Status.online, activity=None)
-    await client.change_presence(activity=discord.Status(name='Running 24/7'))
+    await client.change_presence(status=discord.Status.online, activity=None)
     history = await client.get_channel(article2ID).history().flatten()
     for article in history:
         contentID = str(article.content).split('\n')[0]
